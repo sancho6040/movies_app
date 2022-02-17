ee765e8c660e9a2fd9e26a1444b4821c52c49650
sancho6040 <56619610+sancho6040@users.noreply.github.com>
2022-02-17 14:31:13 -0500

    Update Details.jsx

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,15 +1,49 @@
-import React, { useContext } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useContext, useRef } from 'react';
+import { useParams, Navigate } from 'react-router-dom';
 import { moviesContext } from '../context/MoviesContext';
 import Movie from '../components/Movie';
 
 export default function Details() {
     const {id} = useParams()
-    const {movies} = useContext(moviesContext)
-
+    const {movies, reviews, addReview, loading} = useContext(moviesContext)
+    const comentario = useRef()
+    const rating = useRef()
+    
     const movie = movies.filter(movie=>movie.id==id)[0]
+    
+    if(!movie && !loading){
+    return <Navigate to="/"/>
+    }
+    
+    const add = ()=>{
+    let comment = comentario.current.value
+    let stars = rating.current.value
+    addReview(movie,stars,comment)
+    }
 
-  return <>
-    <Movie movie={movie}/>
-  </>
+  return loading?<p>Loading...</p>:<div>
+      <p>Details {id}</p>
+      <Movie movie={movie}></Movie>
+      {console.log(reviews)}
+      <div>
+        <input ref={comentario} type="text"></input>
+        <select ref={rating}>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+        <button onClick={add}>Agregar review</button>
+      </div>
+
+      {/* && (and): Operador de cortocircuito */}
+      {/* || (or)*/}
+      {reviews.map(
+        review=>review.idMovie===id
+        &&<p key={review.id}>{review.comment}</p>)
+      }
+
+      {/* Mostrar comentarios */}
+  </div>;
 }
