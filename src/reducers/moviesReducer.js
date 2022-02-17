2a79429bf621d211caf4e1e433ed224e2b457be4
sancho6040 <56619610+sancho6040@users.noreply.github.com>
2022-02-17 14:23:23 -0500

    addmovies

--- src/reducers/moviesReducer.js
+++ src/reducers/moviesReducer.js
@@ -5,13 +5,20 @@ export const moviesInitialState = {
 export default function moviesReducer(state, action){
 
     let newState;
-    switch (action.type) {
+    switch(action.type){
         case 'addStars':
-            const {movie, stars} = action
-            movie.stars += parseInt(stars)
+            const {movie,stars} = action
+            movie.stars = movie.stars + parseInt(stars)
             movie.numberOfReviews++
-            newState = {movies: [...state.movies]}
+            newState = {movies:[...state.movies]}
             break;
+        case 'addMovies':
+            const {movies} = action
+            newState = {movies}
+            break;
+        default:
+            newState = state;
     }
+    
     return newState
 }
